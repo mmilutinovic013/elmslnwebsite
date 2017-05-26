a5377416a3375d0891f09d667c21774ab1f77004
Mark Milutinovic <mmilutinovic1313@gmail.com>
2017-05-26 11:07:52 -0400

    Changes

--- update-demo.sh
+++ update-demo.sh
@@ -1,6 +1,6 @@
 # git clone the Polymer tools repository somewhere outside of your 
 # element project
-git clone git://github.com/mmilutinovic1313/elmslnwebsite.git
+git clone git://github.com/Polymer/tools.git
 
 # Create a temporary directory for publishing your element and cd into it
 mkdir temp && cd temp
